d6d99571e0d2e0003d4faa891498ae7147c413fc
Theophilus Tettey <theophilus.tettey@amalitech.org>
2022-01-11 11:09:19 +0000

    My first commit

---
+++ Dog Years.js
@@ -0,0 +1,22 @@
+// My Age
+const myAge = 29;
+// Early Years
+let earlyYears = 2;
+
+earlyYears *= 10.5
+
+let laterYears = myAge - 2;
+
+//Multiply laterYears by 4
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+
+// Add early Years and Later Years to the
+myAgeInDogYears = earlyYears + laterYears;
+
+// My name in lower case
+myName = 'Theophilus Tettey'.toLowerCase;
+
+// Print the string and myName, myAge and myAgeInDogYears
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years ols in dog years`);
